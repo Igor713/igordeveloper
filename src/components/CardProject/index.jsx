d7fb0e09f6d913fd6a63585c489e79e555148565
Igor713 <igormatheussiquieri@gmail.com>
2022-08-15 17:53:45 -0300

    Criado estilo project card

--- src/components/CardProject/index.jsx
+++ src/components/CardProject/index.jsx
@@ -1,14 +1,22 @@
+import SocialButtons from '../SocialButtons';
 import './styles.sass';
 
-const CardProject = ({ cardImage, cardText, cardTechnologies, cardLink }) => {
+const CardProject = ({ cardImage, cardText }) => {
     return (
         <div className="card-container">
-            <div className="card-image">
-                <img src={cardImage} alt="Imagem do projeto" />
+            <div className="card-wrapper">
+                <div className="card-image">
+                    <img src={cardImage} alt="Imagem do projeto" />
+                </div>
+                <div>
+                    <p className="card-text">{cardText}</p>
+                    <SocialButtons
+                        link={"#"}
+                        className={"project-button"}
+                        name={"Veja mais"}
+                    />
+                </div>
             </div>
-            <p className="card-text">{cardText}</p>
-            <div className="card-technologies">{cardTechnologies}</div>
-            <a href={cardLink} className="card-link">a</a>
         </div>
     );
 };
