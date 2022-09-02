1c2cee3b932a97ab0442615d56e1bcdc1fceca9c
igorm <igormatheussiquieri@gmail.com>
2022-09-02 00:05:09 -0300

    Criado botáo toggle menu mobile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,24 @@
 import Menu from "../Menu";
-
 import './styles.sass';
-
 import Brand from '../../assets/icon/letter-i.png';
+import { useState } from "react";
 
 const Header = () => {
+
+    const [isActive, setIsActive] = useState(false);
+
+    const handleClick = (e) => {
+        setIsActive(current => !current);
+    }
+
     return (
         <header>
             <div className="header-container">
                 <div className="brand">
                     <img src={Brand} alt="Logo" />
+                </div>
+                <div className={isActive ? 'header-mobile-btn' : 'header-mobile-btn open'} onClick={handleClick}>
+                    <div className="menu-btn"></div>
                 </div>
                 <div className="header-navigation">
                     <Menu
